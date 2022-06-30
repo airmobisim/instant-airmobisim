9f4ab9bcc5bf11235052a8427fa65f97fe8b1207
Simon Welzel <welzel@cms-lab08.cs.upb.de>
2022-06-30 13:49:53 +0200

    Added formatting in scripts/pre.sh

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -7,7 +7,7 @@ apt -y update
 apt -y install ansible
 
 echo "==> Add user airmobisim to password-less sudoers"
-echo "airmobisim   ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/airmobisim
+echo "airmobisim        ALL=(ALL)        NOPASSWD: ALL" > /etc/sudoers.d/airmobisim
 
 echo "==> Create directories to hold uploaded files"
 mkdir -p ~airmobisim/src; chown airmobisim:airmobisim ~airmobisim/src
