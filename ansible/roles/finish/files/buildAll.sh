bb3c271952008e113640bf6a404af235ea2e3e08
Tobias Hardes <tobias.hardes@uni-paderborn.de>
2022-07-08 19:54:38 +0200

    Fix buildAll script

--- ansible/roles/finish/files/buildAll.sh
+++ ansible/roles/finish/files/buildAll.sh
@@ -31,19 +31,30 @@ make -j$(( $(nproc) - 1 ))
 xvfb-run ~/src/omnetpp/ide/omnetpp -data ~/workspace.omnetpp -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import .
 
 # Import Veins_INET into Workspace
-cd /home/airmobisim/src/veins/subprojects/veins_inet
-xvfb-run ~/src/omnetpp/ide/omnetpp -data ~/workspace.omnetpp -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import .
+#cd /home/airmobisim/src/veins/subprojects/veins_inet
+#xvfb-run ~/src/omnetpp/ide/omnetpp -data ~/workspace.omnetpp -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import .
 
 # Build AirMobiSim
 cd /home/airmobisim/src/
 rm -rf AirMobiSim_libveins
 rm -rf AirMobiSim
-git clone https://github.com/airmobisim/AirMobiSim_libveins
-git clone https://github.com/airmobisim/AirMobiSim.git
+git clone --branch veins-5.2 https://github.com/sommer/veins.git veins
+git clone --branch v0.1 https://git.cms-labs.org/git/hardes/AirMobiSim_libveins
+git clone --branch v0.1 https://git.cms-labs.org/git/hardes/AirMobiSim.git
+#git clone --branch v0.1 https://github.com/airmobisim/AirMobiSim_libveins
+#git clone --branch v0.1 https://github.com/airmobisim/AirMobiSim.git
+
+# Import AirMobiSim_libveins into Workspace
+cd /home/airmobisim/src/AirMobiSim_libveins
+xvfb-run ~/src/omnetpp/ide/omnetpp -data ~/workspace.omnetpp -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -import .
+
 cd /home/airmobisim/src/AirMobiSim
+
 
 source setenv
 ./build.sh -y
+
+
 
 # Clean up after installation was successful - Remove buildAll.desktop file
 rm -f /home/airmobisim/.config/autostart/buildAll.desktop
